7c6a5603b925ceca288215bc2518d96ade5dc0ba
Deep Sant <deepsant@Deeps-MacBook-Pro.local>
2023-05-15 15:32:23 +0530

    new comment [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import MainContent from './components/MainContent';
 import logo from './assets/images/logo.png';
 // github actions somthing
+
+// somthing comment has been added
 function App() {
   return (
     <>
